b5b91d39c48ea5661f78263665d6504b0a2aeb60
Megan <mallen8@DESKTOP-1ECFP69.localdomain>
2021-10-03 17:31:33 -0400

    small edits to formatting

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 os.system('clear')
 from models.add import *
 from models.answer import *
-from models.see import *
 from models.question import *
 from models.sql import *
 from init_db import *
@@ -16,14 +15,12 @@ print("%%%%%% S U R V E Y %%%%%%%%")
 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 
 
-
-
 while True:
     print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
     print('''What would you like to do? (Type exit to quit):
-                        (1) Create a Poll
-                        (2) Launch a Poll
-                        (3) View the Answers of a Poll''')
+            (1) Create a Poll
+            (2) Launch a Poll
+            (3) View the Answers of a Poll''')
     print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
     choice = input("Enter the Number Corresponding to your choice: ")
     try:
@@ -148,9 +145,6 @@ while True:
                                 s.commit()
                             else:
                                 print("Please enter '1' or '2' as your response")
-                            a = s.query(ActualAnswer).all()
-                            for i in a:
-                                print(i.id, i.qid, i.answer, i.answer1, i.answer2, i.answer3)
 
                         else:
                             print("Answer the following question?")
@@ -198,17 +192,17 @@ while True:
             sum2 = 0
             sum3 = 0
             while True:
-                seeresults = str(input("Are you ready to see the results of your poll?")).capitalize()
+                seeresults = str(input("Would you like to see the results of your poll? (Y/N) ")).capitalize()
                 if seeresults == "Y":
                     print("Results are available for the following polls: ")
                     a = s.query(Question1).all()
                     for i in a:
                         print(i.id, i.rt)
-                    whichresults = int(input("Which poll results would you like to see today? Enter the corresponding ID (int) "))
+                    whichresults = int(
+                        input("Which poll results would you like to see today? Enter the corresponding ID (int) "))
 
-                    for i in range(0,len(s.query(ActualAnswer).all())):
+                    for i in range(0, len(s.query(ActualAnswer).all())):
                         if s.query(ActualAnswer)[i].qid == whichresults:
-                            print(s.query(ActualAnswer)[i].qid, whichresults)
                             sum0 = sum0 + s.query(ActualAnswer)[i].answer
                             sum1 = sum1 + s.query(ActualAnswer)[i].answer1
                             sum2 = sum2 + s.query(ActualAnswer)[i].answer2
@@ -216,17 +210,41 @@ while True:
                         else:
                             pass
 
+                    sumall = sum0 + sum1 + sum2 + sum3
+                    perc0 = round((sum0 / sumall) * 100, 2)
+                    perc0 = str(perc0) + "%"
+                    perc1 = round((sum1 / sumall) * 100, 2)
+                    perc1 = str(perc1) + "%"
+                    perc2 = round((sum2 / sumall) * 100, 2)
+                    perc2 = str(perc2) + "%"
+                    perc3 = round((sum3 / sumall) * 100, 2)
+                    perc3 = str(perc3) + "%"
 
-                        sumall = sum0 + sum1 + sum2 + sum3
-                        print(sum0, sum1, sum2, sum3, sumall)
+                    if (s.query(PotentialAnswer)[whichresults - 1].name2 == None):
+                        dicti = {s.query(PotentialAnswer)[whichresults - 1].name: perc0,
+                                 s.query(PotentialAnswer)[whichresults - 1].name1: perc1}
+                        print(dicti)
 
-                    #results = s.query(ActualAnswer).get(whichresults)
-                    #print(results)
-                    #sum = s.query(ActualAnswer).answer()
-                    #sum1 = s.query(ActualAnswer).answer1()
-                    #sum2 = s.query(ActualAnswer).answer2()
-                    #sum3 = s.query(ActualAnswer).answer3()
-                    #print(sum, sum1, sum2, sum3)
+                    elif (s.query(PotentialAnswer)[whichresults - 1].name3 == None):
+                        dicti = {s.query(PotentialAnswer)[whichresults - 1].name: perc0,
+                                 s.query(PotentialAnswer)[whichresults - 1].name1: perc1,
+                                 s.query(PotentialAnswer)[whichresults - 1].name2: perc2}
+                        print(dicti)
+
+                    else:
+                        dicti = {s.query(PotentialAnswer)[whichresults - 1].name: perc0,
+                                 s.query(PotentialAnswer)[whichresults - 1].name1: perc1,
+                                 s.query(PotentialAnswer)[whichresults - 1].name2: perc2,
+                                 s.query(PotentialAnswer)[whichresults - 1].name3: perc3}
+                        print(dicti)
+
+                    # results = s.query(ActualAnswer).get(whichresults)
+                    # print(results)
+                    # sum = s.query(ActualAnswer).answer()
+                    # sum1 = s.query(ActualAnswer).answer1()
+                    # sum2 = s.query(ActualAnswer).answer2()
+                    # sum3 = s.query(ActualAnswer).answer3()
+                    # print(sum, sum1, sum2, sum3)
 
                     break
                 elif seeresults == "N":
@@ -241,69 +259,5 @@ while True:
         else:
             print("Enter a valid number")
 
-while True:
-    seeresults = str(input("Would you like to see the results of your poll? (Y/N) ")).capitalize()
-    if seeresults == "Y":
-        print("Results are available for the following polls: ")
-        a = s.query(Question1).all()
-        for i in a:
-            print(i.id, i.rt)
-        whichresults = int(input("Which poll results would you like to see today? Enter the corresponding ID (int) "))
-
-        for i in range(0,len(s.query(ActualAnswer).all())):
-            if s.query(ActualAnswer)[i].qid == whichresults:
-                sum0 = sum0 + s.query(ActualAnswer)[i].answer
-                sum1 = sum1 + s.query(ActualAnswer)[i].answer1
-                sum2 = sum2 + s.query(ActualAnswer)[i].answer2
-                sum3 = sum3 + s.query(ActualAnswer)[i].answer3
-            else:
-                pass
 
 
-        sumall = sum0 + sum1 + sum2 + sum3
-        perc0 = round((sum0 / sumall) * 100, 2)
-        perc0 = str(perc0) + "%"
-        perc1 = round((sum1 / sumall) * 100, 2)
-        perc1 = str(perc1) + "%"
-        perc2 = round((sum2 / sumall) * 100, 2)
-        perc2 = str(perc2) + "%"
-        perc3 = round((sum3 / sumall) * 100, 2)
-        perc3 = str(perc3) + "%"
-
-
-        if (s.query(PotentialAnswer)[whichresults-1].name2 == None):
-            dicti = {s.query(PotentialAnswer)[whichresults-1].name: perc0,
-                     s.query(PotentialAnswer)[whichresults-1].name1: perc1}
-            print(dicti)
-
-        elif (s.query(PotentialAnswer)[whichresults-1].name3 == None):
-            dicti = {s.query(PotentialAnswer)[whichresults-1].name: perc0,
-                     s.query(PotentialAnswer)[whichresults-1].name1: perc1,
-                     s.query(PotentialAnswer)[whichresults-1].name2: perc2}
-            print(dicti)
-
-        else:
-            dicti = {s.query(PotentialAnswer)[whichresults-1].name: perc0,
-                     s.query(PotentialAnswer)[whichresults-1].name1: perc1,
-                     s.query(PotentialAnswer)[whichresults-1].name2: perc2,
-                     s.query(PotentialAnswer)[whichresults-1].name3: perc3}
-            print(dicti)
-
-
-
-
-        #results = s.query(ActualAnswer).get(whichresults)
-        #print(results)
-        #sum = s.query(ActualAnswer).answer()
-        #sum1 = s.query(ActualAnswer).answer1()
-        #sum2 = s.query(ActualAnswer).answer2()
-        #sum3 = s.query(ActualAnswer).answer3()
-        #print(sum, sum1, sum2, sum3)
-
-        break
-    elif seeresults == "N":
-        print("Ok! have a nice day")
-        break
-    else:
-        print("Please enter 'Y' or 'N' as a response")
-        continue
